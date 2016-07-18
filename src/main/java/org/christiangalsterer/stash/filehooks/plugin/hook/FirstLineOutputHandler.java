3de3c5c44740427772b6297278d4bfc3e515b6ef
unknown <ellaz@ELLAZ-PC.bigworldtech.com>
2016-07-18 10:19:10 +1000

    Add merge request check The original plugin didn't apply hook on a merge request from a repository without hook settings to one with

---
+++ src/main/java/org/christiangalsterer/stash/filehooks/plugin/hook/FirstLineOutputHandler.java
@@ -0,0 +1,40 @@
+package org.christiangalsterer.stash.filehooks.plugin.hook;
+
+import java.io.IOException; 
+
+import javax.annotation.Nullable; 
+ 
+import com.atlassian.bitbucket.io.LineReader; 
+import com.atlassian.bitbucket.io.LineReaderOutputHandler; 
+import com.atlassian.bitbucket.scm.CommandOutputHandler; 
+ 
+/**
+ * Returns the first line of output provided by the git process. 
+ */ 
+public class FirstLineOutputHandler extends LineReaderOutputHandler implements 
+        CommandOutputHandler<String> { 
+ 
+    private String sha; 
+ 
+    public FirstLineOutputHandler() { 
+        super("UTF-8"); 
+    } 
+ 
+    @Nullable 
+    @Override 
+    public String getOutput() { 
+        return sha; 
+    } 
+ 
+    @Override 
+    protected void processReader(LineReader lineReader) throws IOException { 
+        sha = lineReader.readLine(); 
+ 
+        // ignore the rest of the output 
+        String nextLine = lineReader.readLine(); 
+        while (nextLine != null) { 
+            nextLine = lineReader.readLine(); 
+        } 
+    } 
+ 
+}
---
+++ src/main/java/org/christiangalsterer/stash/filehooks/plugin/hook/GitUtils.java
@@ -0,0 +1,23 @@
+package org.christiangalsterer.stash.filehooks.plugin.hook;
+
+import com.atlassian.bitbucket.repository.Repository; 
+import com.atlassian.bitbucket.scm.CommandBuilderSupport; 
+import com.atlassian.bitbucket.scm.git.GitScmConfig; 
+ 
+/**
+ * Git Utilities class. 
+ * @author Pavel Baranchikov 
+ */ 
+public class GitUtils { 
+ 
+    private GitUtils() { 
+    } 
+ 
+    public static void setAlternateIfCrossRepository(CommandBuilderSupport<?> builder, 
+            Repository repository, Repository secondRepository, GitScmConfig config) { 
+        if (repository.getId() != secondRepository.getId()) { 
+            builder.environment("GIT_ALTERNATE_OBJECT_DIRECTORIES", 
+                    config.getObjectsDir(secondRepository).getAbsolutePath()); 
+        } 
+    } 
+}
